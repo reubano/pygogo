01ee8e76538ba77295037ad52138adb16c53d109
Reuben Cummings <reubano@gmail.com>
2015-12-07 16:26:13 +0300

    Update tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,13 +13,20 @@ from __future__ import (
     unicode_literals)
 
 import nose.tools as nt
+import sys
+
+from StringIO import StringIO
+from gogo import handlers
+from gogo.logger import Logger
+
+logger = Logger(__name__).logger
 
 
 def setup_module():
     """site initialization"""
     global initialized
     initialized = True
-    print('Site Module Setup\n')
+    logger.debug('Main module setup\n')
 
 
 class TestMain:
@@ -27,5 +34,28 @@ class TestMain:
     def __init__(self):
         self.cls_initialized = False
 
-    def test_home(self):
-        nt.assert_equal(self.cls_initialized, False)
+    def setUp(self):
+        nt.assert_false(self.cls_initialized)
+        self.cls_initialized = True
+        logger.debug('TestMain class setup\n')
+
+    def tearDown(self):
+        nt.ok_(self.cls_initialized)
+        logger.debug('TestMain class teardown\n')
+
+    def test_handlers(self):
+        f = StringIO()
+        hdlr = handlers.fileobj_hdlr(f)
+        lggr = Logger('test_handlers', high_pass_hdlr=hdlr).logger
+
+        msg1 = 'stdout hdlr only'
+        lggr.debug(msg1)
+        f.seek(0)
+        nt.assert_equal(sys.stdout.getvalue().strip(), msg1)
+        nt.assert_false(f.read())
+
+        msg2 = 'both hdlrs'
+        lggr.error(msg2)
+        f.seek(0)
+        nt.assert_equal(sys.stdout.getvalue().strip(), '%s\n%s' % (msg1, msg2))
+        nt.assert_equal(f.read().strip(), msg2)
