8e58cd1afd4d2160993a679eece4843542f117f7
Reuben Cummings <reubano@gmail.com>
2015-12-07 17:53:33 +0300

    Bump to version 0.2.0

--- gogo/__init__.py
+++ gogo/__init__.py
@@ -20,7 +20,7 @@ from __future__ import (
     absolute_import, division, print_function, with_statement,
     unicode_literals)
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 __title__ = 'gogo'
 __author__ = 'Reuben Cummings'
