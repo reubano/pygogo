09e6a48713ad99bbb3617175a4eacb3f66800708
Reuben Cummings <reubano@gmail.com>
2016-01-01 13:48:25 +0300

    Bump to version 0.8.12

--- pygogo/__init__.py
+++ pygogo/__init__.py
@@ -53,7 +53,7 @@ from copy import copy
 from builtins import *
 from . import formatters, handlers, utils
 
-__version__ = '0.8.11'
+__version__ = '0.8.12'
 
 __all__ = ['formatters', 'handlers', 'utils']
 __title__ = 'pygogo'
