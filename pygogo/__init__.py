0715fdade99a6bd36350aeb18729793112feed21
Reuben Cummings <reubano@gmail.com>
2018-04-05 02:47:27 +0300

    Bump to version 0.11.0

--- pygogo/__init__.py
+++ pygogo/__init__.py
@@ -52,7 +52,7 @@ from copy import copy
 from builtins import *
 from . import formatters, handlers, utils
 
-__version__ = '0.10.0'
+__version__ = '0.11.0'
 
 __all__ = ['formatters', 'handlers', 'utils']
 __title__ = 'pygogo'
