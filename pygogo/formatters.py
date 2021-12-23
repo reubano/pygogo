cab06d77b1493fb19f64f0b7869ead72ab70a3d7
Reuben Cummings <reubano@gmail.com>
2021-12-23 00:20:32 -0600

    [FIX] Use real csv formatter (closes #18)

--- pygogo/formatters.py
+++ pygogo/formatters.py
@@ -46,8 +46,6 @@ Attributes:
 
     FIXED_FORMAT (str): A fixed width format
 
-    CSV_FORMAT (str): A csv format
-
     JSON_FORMAT (str): A json format
 
     DATEFMT (str): Standard date format
@@ -57,14 +55,15 @@ import logging
 import sys
 import traceback
 import itertools as it
+import csv
 
+from io import StringIO
 from .utils import CustomEncoder
 
 BASIC_FORMAT = "%(message)s"
 BOM_FORMAT = "\ufeff%(message)s"
 CONSOLE_FORMAT = "%(name)-12s: %(levelname)-8s %(message)s"
 FIXED_FORMAT = "%(asctime)s.%(msecs)-3d %(name)-12s %(levelname)-8s %(message)s"
-CSV_FORMAT = '%(asctime)s.%(msecs)d,%(name)s,%(levelname)s,"%(message)s"'
 JSON_FORMAT = (
     '{"time": "%(asctime)s.%(msecs)d", "name": "%(name)s", "level":'
     ' "%(levelname)s", "message": "%(message)s"}'
@@ -77,7 +76,132 @@ module_logger = logging.getLogger(__name__)
 module_logger.addHandler(module_hdlr)
 
 
-class StructuredFormatter(logging.Formatter):
+class BaseFormatter(logging.Formatter):
+    def __init__(self, fmt=None, datefmt=None, **kwargs):
+        """Initialization method.
+
+        Args:
+            fmt (string): Log message format.
+
+            datefmt (dict): Log date format.
+
+        Returns:
+            New instance of :class:`BaseFormatter`
+
+        Examples:
+            >>> BaseFormatter("%(message)s")  # doctest: +ELLIPSIS
+            <pygogo.formatters.BaseFormatter object at 0x...>
+        """
+        empty_record = logging.makeLogRecord({})
+        filterer = lambda k: k not in empty_record.__dict__ and k != "asctime"
+        self.filterer = filterer
+        super().__init__(datefmt=datefmt)
+
+
+# https://stackoverflow.com/a/19766056/408556
+class CsvFormatter(BaseFormatter):
+    """A logging formatter that creates a csv string from log details
+
+    Args:
+        fmt (string): Log message format.
+
+        datefmt (dict): Log date format.
+
+    Returns:
+        New instance of :class:`CsvFormatter`
+
+    Examples:
+        >>> from io import StringIO
+        >>>
+        >>> s = StringIO()
+        >>> logger = logging.getLogger()
+        >>> formatter = CsvFormatter(datefmt=DATEFMT)
+        >>> hdlr = logging.StreamHandler(s)
+        >>> hdlr.setFormatter(formatter)
+        >>> logger.addHandler(hdlr)
+        >>> logger.info('hello "world"')
+        >>> s.getvalue().strip()  # doctest: +ELLIPSIS
+        '"20...","root","INFO","hello \"\"world\"\"","hello \"\"world\"\""'
+    """
+
+    def __init__(self, fmt=None, datefmt=None, quoting=csv.QUOTE_ALL, **kwargs):
+        """Initialization method.
+
+        Args:
+            fmt (string): Log message format.
+
+            datefmt (dict): Log date format.
+
+        Returns:
+            New instance of :class:`CsvFormatter`
+
+        Examples:
+            >>> CsvFormatter("%(message)s")  # doctest: +ELLIPSIS
+            <pygogo.formatters.CsvFormatter object at 0x...>
+        """
+        super().__init__(fmt=fmt, datefmt=datefmt)
+        self.output = StringIO()
+        self.writer = csv.writer(self.output, quoting=quoting, **kwargs)
+
+    def format(self, record):
+        """ Formats a record as a dict string
+
+        Args:
+            record (object): The event to format.
+
+        Returns:
+            str: The formatted content
+
+        Examples:
+            >>> formatter = CsvFormatter(datefmt='%Y')
+            >>> logger = logging.getLogger()
+            >>> args = (logging.INFO, '.', 0, 'hello "world"', [], None)
+            >>> record = logger.makeRecord('root', *args)
+            >>> formatter.format(record)  # doctest: +ELLIPSIS
+            '"20...","root","INFO","hello \"\"world\"\""'
+        """
+        row = [self.formatTime(record, self.datefmt), record.name, record.levelname]
+        keys = filter(self.filterer, record.__dict__)
+        extra = [record.__dict__[k] for k in keys]
+
+        self.writer.writerow(row + extra + [record.getMessage()])
+        data = self.output.getvalue()
+        self.output.truncate(0)
+        self.output.seek(0)
+        return data.strip()
+
+    def formatException(self, exc_info):
+        """Formats an exception as a dict string
+
+        Args:
+            exc_info (tuple[type, value, traceback]): Exception tuple as
+                returned by `sys.exc_info()`
+
+        Returns:
+            str: The formatted exception
+
+        Examples:
+            >>> formatter = CsvFormatter()
+            >>>
+            >>> try:  # doctest: +ELLIPSIS
+            ...     1 / 0
+            ... except:
+            ...     formatter.formatException(sys.exc_info())
+            '"ZeroDivisionError","division by zero","<docte...>","2","<module>","1 / 0"'
+        """
+        type_, value, trcbk = exc_info
+
+        # ["type", "value", "filename", "lineno", "function", "text"]
+        self.writer.writerow(
+            it.chain([type_.__name__, value], *traceback.extract_tb(trcbk))
+        )
+        data = self.output.getvalue()
+        self.output.truncate(0)
+        self.output.seek(0)
+        return data.strip()
+
+
+class StructuredFormatter(BaseFormatter):
     """A logging formatter that creates a json string from log details
 
     Args:
@@ -91,10 +215,10 @@ class StructuredFormatter(logging.Formatter):
     Examples:
         >>> from io import StringIO
         >>> from json import loads
-
+        >>>
         >>> s = StringIO()
         >>> logger = logging.getLogger()
-        >>> formatter = StructuredFormatter(BASIC_FORMAT, datefmt=DATEFMT)
+        >>> formatter = StructuredFormatter(datefmt=DATEFMT)
         >>> hdlr = logging.StreamHandler(s)
         >>> hdlr.setFormatter(formatter)
         >>> logger.addHandler(hdlr)
@@ -108,26 +232,6 @@ class StructuredFormatter(logging.Formatter):
         True
     """
 
-    def __init__(self, fmt=None, datefmt=None):
-        """Initialization method.
-
-        Args:
-            fmt (string): Log message format.
-
-            datefmt (dict): Log date format.
-
-        Returns:
-            New instance of :class:`StructuredFormatter`
-
-        Examples:
-            >>> StructuredFormatter("%(message)s")  # doctest: +ELLIPSIS
-            <pygogo.formatters.StructuredFormatter object at 0x...>
-        """
-        empty_record = logging.makeLogRecord({})
-        filterer = lambda k: k not in empty_record.__dict__ and k != "asctime"
-        self.filterer = filterer
-        super(StructuredFormatter, self).__init__(fmt, datefmt)
-
     def format(self, record):
         """ Formats a record as a dict string
 
@@ -139,8 +243,8 @@ class StructuredFormatter(logging.Formatter):
 
         Examples:
             >>> from json import loads
-
-            >>> formatter = StructuredFormatter(BASIC_FORMAT, datefmt='%Y')
+            >>>
+            >>> formatter = StructuredFormatter(datefmt='%Y')
             >>> logger = logging.getLogger()
             >>> args = (logging.INFO, '.', 0, 'hello world', [], None)
             >>> record = logger.makeRecord('root', *args)
@@ -204,6 +308,6 @@ basic_formatter = logging.Formatter(BASIC_FORMAT)
 bom_formatter = logging.Formatter(BOM_FORMAT)
 console_formatter = logging.Formatter(CONSOLE_FORMAT)
 fixed_formatter = logging.Formatter(FIXED_FORMAT, datefmt=DATEFMT)
-csv_formatter = logging.Formatter(CSV_FORMAT, datefmt=DATEFMT)
+csv_formatter = CsvFormatter(BASIC_FORMAT, datefmt=DATEFMT)
 json_formatter = logging.Formatter(JSON_FORMAT, datefmt=DATEFMT)
 structured_formatter = StructuredFormatter(BASIC_FORMAT, datefmt=DATEFMT)
--- tests/test_main.py
+++ tests/test_main.py
@@ -59,7 +59,7 @@ class TestMain(BaseTest):
         # csv_formatter
         formatter = gogo.formatters.csv_formatter
         csv_logger = gogo.Gogo("csv", low_formatter=formatter).logger
-        csv_logger.debug("hello")
+        csv_logger.debug('hello "world"')
 
         # console_formatter
         formatter = gogo.formatters.console_formatter
@@ -67,8 +67,10 @@ class TestMain(BaseTest):
         console_lggr.debug("hello")
 
         console_msg = (
-            'stdout\nstderr\n{"time": "20...", "name": "json.base", "level": '
-            '"DEBUG", "message": "hello"}\n20...,csv.base,DEBUG,"hello"\n'
+            "stdout\n"
+            "stderr\n"
+            '{"time": "20...", "name": "js...", "level": "DEBUG", "message": "hello"}\n'
+            '"20...","csv.base","DEBUG","hello ""world"""\n'
             "console.base: DEBUG    hello"
         )
 
